6583cfe0c8ea351a3642240d503cd86a63abbe39
ilyaDubovtsev <dubovtsev98@gmail.com>
2023-05-28 17:04:33 +0400

    Update

---
+++ FluentAPI/HttpTask/FluentAssertion/FluentTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+// ReSharper disable once RedundantUsingDirective
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FluentAssertionTask
+{
+    public class FluentTests
+    {
+        /// <summary>
+        /// https://fluentassertions.com/documentation/
+        /// </summary>
+        //Реализуйте тесты, повторяющие поведение тестов в классе AssertionTests, но используйте при этом ассерты из FluentAssertions
+        [Test]
+        public void TestObjectsAreEqual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void TestObjectReferencesAreEqual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void TestBooleanObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public async Task TestCanWorkWithExceptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void TestStringAssertions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void TestDecimalAssertions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void TestCollectionsAssertions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FluentAPI/HttpTask/LogicStringBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EncapsulationTask
+{
+    public class LogicStringBuilder : ILogicStringBuilder
+    {
+        //Представьте, что вам понадобился логинческий стрингбилдер,
+        //который умеет добавлять строки в конец только при выоплнении каких-то условий
+        //можно усложнить методами типа And() Or() и их комбинациями
+        //постарайтесь использовать меньше памяти
+        public LogicStringBuilder(string str)
+        {
+            
+        }
+
+        public ILogicStringBuilder IfEndsWith(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILogicStringBuilder IfStartsWith(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILogicStringBuilder Append(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILogicStringBuilder AddToStart(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
